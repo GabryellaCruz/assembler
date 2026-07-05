a0eb77e1278c393ccbd28b9ac4c8bce5bebfbbc4
Mateus Dutra <mateus.dutra.vale.dv@gmail.com>
2026-07-05 13:19:46 -0300

    feat(main): implementa segunda passagem e geracao de arquivos binarios .hack

--- src/main/java/org/ufma/Assembler.java
+++ src/main/java/org/ufma/Assembler.java
@@ -1,10 +1,11 @@
-package org.ufma.main;
+package org.ufma;
 
 import org.ufma.parser.Parser;
 import org.ufma.parser.CommandType;
 import org.ufma.symbols.SymbolTable;
 import org.ufma.code.Code;
 
+import java.io.PrintWriter;
 import java.io.IOException;
 
 public class Assembler {
@@ -15,11 +16,11 @@ public class Assembler {
         }
 
         String inputFilename = args[0];
+        String outputFilename = inputFilename.replace(".asm", ".hack");
 
         try {
             SymbolTable symbolTable = new SymbolTable();
 
-            // === PRIMEIRA PASSAGEM: Coletar rotulos (Labels) ===
             Parser firstPassParser = new Parser(inputFilename);
             int romAddress = 0;
 
@@ -30,11 +31,48 @@ public class Assembler {
                 if (type == CommandType.L_COMMAND) {
                     symbolTable.addEntry(firstPassParser.symbol(), romAddress);
                 } else {
-                    romAddress++; // Apenas instrucoes A e C ocupam espaco real na ROM
+                    romAddress++;
                 }
             }
 
-            System.out.println("Primeira passagem concluida! Rotulos mapeados.");
+            Parser secondPassParser = new Parser(inputFilename);
+            try (PrintWriter out = new PrintWriter(outputFilename)) {
+                while (secondPassParser.hasMoreInstructions()) {
+                    secondPassParser.advance();
+                    CommandType type = secondPassParser.commandType();
+
+                    if (type == CommandType.A_COMMAND) {
+                        String symbol = secondPassParser.symbol();
+                        int value;
+
+                        if (symbol.matches("\\d+")) {
+                            // Se for constante numerica pura (ex: @100)
+                            value = Integer.parseInt(symbol);
+                        } else {
+                            // Se for uma variavel ou um rotulo mapeado (ex: @sum)
+                            if (!symbolTable.contains(symbol)) {
+                                symbolTable.addVariable(symbol);
+                            }
+                            value = symbolTable.getAddress(symbol);
+                        }
+
+                        // Formata para string binaria de 15 bits preenchida com 0 a esquerda + o bit '0' indicador de instrucao A
+                        String binaryA = "0" + String.format("%15s", Integer.toBinaryString(value)).replace(' ', '0');
+                        out.println(binaryA);
+
+                    } else if (type == CommandType.C_COMMAND) {
+                        String compBin = Code.comp(secondPassParser.comp());
+                        String destBin = Code.dest(secondPassParser.dest());
+                        String jumpBin = Code.jump(secondPassParser.jump());
+
+                        // Monta o frame completo da instrucao C: 111 + comp + dest + jump
+                        String binaryC = "111" + compBin + destBin + jumpBin;
+                        out.println(binaryC);
+                    }
+                }
+            }
+
+            System.out.println(" ARQUIVO BINARIO .HACK GERADO COM SUCESSO!");
 
         } catch (IOException e) {
             System.out.println("Erro no processamento: " + e.getMessage());
